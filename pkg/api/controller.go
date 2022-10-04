2d15cef7ca979ded2b09740ad480a121aaa161f3
Tim Richardson <trichardsonjr@gmail.com>
2022-10-03 21:49:52 -0700

    wip

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -14,10 +14,17 @@ func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 		DB: db,
 	}
 
-	routes := r.Group("/api")
+	routes := r.Group("/api/v1/")
 	routes.GET("/", h.GetRecords)
+	routes.GET("/clusters", h.GetClusters)
+	routes.GET("/cluster/:cluster_id", h.GetClustersResources)
 	routes.GET("/cluster_id/:cluster_id", h.GetUuidByClusterID)
+	routes.GET("/resource/:resource_uuid/logs/generation/:generation", h.GetClustersResourcesLogs)
+	//routes.GET("/cluster/:cluster_id/resource/:resource_uuid/logs/generation/:generation", h.GetUuidByClusterID)
 	routes.GET("/cluster_name/:cluster_name", h.GeIdByClusterName)
 	routes.GET("/:tfo_resource_uuid", h.GetLog)
 	routes.GET("/logsByGeneration/:tfo_resource_uuid/:generation", h.GetLogByGeneration)
+	routes.GET("/queryRerun/:tfo_resource_uuid/:task_type/:rerun_value/:generation", h.GetRerunByNumber)
+	//routes.GET("/highestRerun/:tfo_resource_uuid/:task_type", h.GetHighestRerunLog)
+	routes.GET("/highestRerun/:generation", h.GetHighestRerunLog)
 }
--- pkg/api/get_record.go
+++ pkg/api/get_record.go
@@ -22,14 +22,14 @@ func (h handler) GetLog(c *gin.Context) {
 func (h handler) GetLogByGeneration(c *gin.Context) {
 	uuid := c.Param("tfo_resource_uuid")
 	generation := c.Param("generation")
-	var gnerationLog models.TFOTaskLog
+	var gnerationLogs []models.TFOTaskLog
 
-	if result := h.DB.Where("generation = ? AND tfo_resource_uuid = ?", &generation, &uuid).First(&gnerationLog); result.Error != nil {
+	if result := h.DB.Where("generation = ? AND tfo_resource_uuid = ?", &generation, &uuid).Find(&gnerationLogs); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
 
-	c.JSON(http.StatusOK, &gnerationLog)
+	c.JSON(http.StatusOK, &gnerationLogs)
 
 }
 
@@ -68,4 +68,122 @@ func (h handler) GetRecords(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, &Records)
+}
+
+func (h handler) GetClusters(c *gin.Context) {
+	var Clusters []models.Cluster
+
+	if result := h.DB.Find(&Clusters); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &Clusters)
+}
+
+func (h handler) GetClustersResources(c *gin.Context) {
+	var Resources []models.TFOResource
+	clusterID := c.Param("cluster_id")
+
+	if result := h.DB.Where("cluster_id = ?", clusterID).Find(&Resources); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &Resources)
+}
+
+func (h handler) GetClustersResourcesLogs(c *gin.Context) {
+	var Logs []models.TFOTaskLog
+	var tfoResource models.TFOResource
+	//clusterID := c.Param("cluster")
+	generation := c.Param("generation")
+	uuid := c.Param("resource_uuid")
+	//var tfoResourceLogs models.TFOTaskLog
+
+	// if result := h.DB.Where("cluster_id = ?", clusterID).Find(&tfoResource); result.Error != nil {
+	// 	c.AbortWithError(http.StatusNotFound, result.Error)
+	// 	return
+	// }
+	// uuid := tfoResource.UUID
+
+	if generation == "latest" {
+		if result := h.DB.First(&tfoResource, "uuid = ?", &uuid); result.Error != nil {
+			c.AbortWithError(http.StatusNotFound, result.Error)
+			return
+		}
+		generation = tfoResource.CurrentGeneration
+	}
+
+	if result := h.DB.Where("tfo_resource_uuid = ? AND generation = ?", &uuid, &generation).Find(&Logs); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &Logs)
+}
+
+func (h handler) GetRerunByNumber(c *gin.Context) {
+	uuid := c.Param("tfo_resource_uuid")
+	taskType := c.Param("task_type")
+	generation := c.Param("generation")
+	var rerunNumbers []models.TFOTaskLog
+	rerunValue := c.Param("rerun_value")
+	var tfoResource models.TFOResource
+	//todo query for latest
+
+	if generation == "latest" {
+		if result := h.DB.First(&tfoResource, "uuid = ?", &uuid); result.Error != nil {
+			c.AbortWithError(http.StatusNotFound, result.Error)
+			return
+		}
+		generation = tfoResource.CurrentGeneration
+	}
+
+	if result := h.DB.Where("task_type = ? AND tfo_resource_uuid = ? AND rerun = ? AND generation = ?", &taskType, &uuid, &rerunValue, &generation).Find(&rerunNumbers); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &rerunNumbers)
+}
+
+// func (h handler) GetHighestRerunLog(c *gin.Context) {
+// 	uuid := c.Param("tfo_resource_uuid")
+// 	taskType := c.Param("task_type")
+// 	var highestRerun models.TFOTaskLog
+// 	rerunValue := 4
+
+// 	// var result int64
+// 	// row := h.DB.Table("tfo_task_logs").Select("message").Where("task_type = ? AND tfo_resource_uuid = ?", &taskType, &uuid).Where("task_type = ? AND tfo_resource_uuid = ?").Select("max(rerun)").Row()
+// 	// row.Scan(&result)
+
+// 	if result := h.DB.Select("message").Where("task_type = ? AND tfo_resource_uuid = ? AND rerun = ?", &taskType, &uuid, &rerunValue).First(&highestRerun); result.Error != nil {
+// 		c.AbortWithError(http.StatusNotFound, result.Error)
+// 		return
+// 	}
+
+// 	c.JSON(http.StatusOK, &highestRerun)
+// }
+
+func (h handler) GetHighestRerunLog(c *gin.Context) {
+	//uuid := c.Param("tfo_resource_uuid")
+	//taskType := c.Param("task_type")
+	generation := c.Param("generation")
+	var highestReruns []models.TFOTaskLog
+	//rerunValue := 4
+
+	// var result int64
+	// row := h.DB.Table("tfo_task_logs").Select("message").Where("task_type = ? AND tfo_resource_uuid = ?", &taskType, &uuid).Where("task_type = ? AND tfo_resource_uuid = ?").Select("max(rerun)").Row()
+	// row.Scan(&result)
+
+	// select max(rerun) from tfo_task_logs where generation = '4';
+	//if result := h.DB.Select("(MAX(rerun))").Where("task_type = ? AND tfo_resource_uuid = ?", &taskType, &uuid).Find(&highestReruns); result.Error != nil {
+	//query := db.Table("order").Select("MAX(order.finished_at) as latest").Joins("left join user user on order.user_id = user.id"
+	if result := h.DB.Table("tfo_task_logs").Select("(MAX(rerun))").Where("generation = ?", &generation).Find(&highestReruns); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &highestReruns)
 }
